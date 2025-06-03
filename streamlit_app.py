2a4651dcc26dbaed5e899a8b8a21d7d46a232b76
Onesime165 <ndriablatie123@gmail.com>
2025-06-03 23:15:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title('🤖 streamlitapp')
 
 st.info('Ceci est une application streamlit')
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/penguins-heroku/refs/heads/master/penguins_example.csv)
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/penguins-heroku/refs/heads/master/penguins_example.csv')
 df
