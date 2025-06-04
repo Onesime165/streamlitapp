892ece881bb8cc9b0cfb0f5f6f29465e57d07a1e
Onesime165 <ndriablatie123@gmail.com>
2025-06-04 12:20:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🤖 Machine learning')
 
 st.info('Ceci est une application streamlit')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/penguins-heroku/refs/heads/master/penguins_example.csv')
-a = df.select_dtypes(include='number')
+a = df.select_dtypes(include='int64')
 a.corr()
