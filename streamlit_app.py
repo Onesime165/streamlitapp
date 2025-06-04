767644412efd33ce0a0ce916b4f5fd320defb36a
Onesime165 <ndriablatie123@gmail.com>
2025-06-04 12:16:28 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title('🤖 Machine learning')
 
 st.info('Ceci est une application streamlit')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/penguins-heroku/refs/heads/master/penguins_example.csv')
-df
+print(df.head())
